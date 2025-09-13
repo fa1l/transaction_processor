7fc399d38eced74984d90c233812f8d1e6266af4
Stepan Pletnev <fa1l@m3pro.local>
2025-09-13 15:16:24 +0200

    add errors

---
+++ src/errors.rs
@@ -0,0 +1,45 @@
+use std::fmt;
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum AccountError {
+    BalanceOverflow,
+    InsufficientMoney,
+    AccountLocked,
+    AccountNotFound,
+}
+
+impl fmt::Display for AccountError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            AccountError::BalanceOverflow => write!(f, "Balance overflow happened"),
+            AccountError::InsufficientMoney => write!(f, "Insufficient money"),
+            AccountError::AccountLocked => write!(f, "Account is locked"),
+            &AccountError::AccountNotFound => write!(f, "Account not found"),
+        }
+    }
+}
+
+impl std::error::Error for AccountError {}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum TransactionError {
+    OriginTransactionNotFound,
+    TransactionNotDisputed,
+    MultipleTransactionDispute,
+}
+
+impl fmt::Display for TransactionError {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self {
+            TransactionError::OriginTransactionNotFound => {
+                write!(f, "Origin transaction not found")
+            }
+            TransactionError::TransactionNotDisputed => write!(f, "Transaction not disputed"),
+            TransactionError::MultipleTransactionDispute => {
+                write!(f, "Multiple transaction dispute")
+            }
+        }
+    }
+}
+
+impl std::error::Error for TransactionError {}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+pub mod errors;
+
 fn main() {
     println!("Hello, world!");
 }
