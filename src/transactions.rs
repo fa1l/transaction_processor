04487af29447fdffed3289b8c0d1fe3e14073c73
Stepan Pletnev <fa1l@m3pro.local>
2025-09-14 13:09:49 +0200

    transaction: changed trait definition & add more transactions

--- src/transactions.rs
+++ src/transactions.rs
@@ -1,18 +1,32 @@
+use enum_dispatch::enum_dispatch;
 use rust_decimal::Decimal;
 use std::error::Error;
 
+const DEPOSIT_VALUE: &str = "deposit";
+const WITHDRAWAL_VALUE: &str = "withdrawal";
+const DISPUTE_VALUE: &str = "dispute";
+const RESOLVE_VALUE: &str = "resolve";
+const CHARGEBACK_VALUE: &str = "chargeback";
+
 use crate::{
-    errors::TransactionError,
-    storage::{Storage, UserId},
+    errors::{TransactionError, TransactionLogError},
+    history::TransactionHistoryStorage,
+    storage::{AccountStorage, ClientId},
+    transactions_processor::TransactionLogEntry,
 };
 
 pub type TransactionId = u64;
 
-pub trait TransactionExecutor {
-    fn execute(self, storage: &mut impl Storage) -> Result<(), Box<dyn Error>>;
+pub trait ExecTransaction {
+    fn execute(
+        &self,
+        account_storage: &impl AccountStorage,
+        history: &impl TransactionHistoryStorage,
+    ) -> Result<(), Box<dyn Error>>;
 }
 
-#[derive(Debug)]
+#[enum_dispatch(ExecTransaction)]
+#[derive(Debug, PartialEq)]
 pub enum Transaction {
     Deposit,
     Withdrawal,
@@ -21,32 +35,138 @@ pub enum Transaction {
     Chargeback,
 }
 
-pub struct Deposit {
-    client_id: UserId,
-    transaction_id: TransactionId,
-    amount: Decimal,
-}
-
-impl TransactionExecutor for Deposit {
-    fn execute(self, storage: &mut impl Storage) -> Result<(), Box<dyn Error>> {
-        if self.amount.is_sign_negative() {
-            return Err(Box::new(TransactionError::NegativeAmount));
+impl ExecTransaction for Transaction {
+    fn execute(
+        &self,
+        account_storage: &impl AccountStorage,
+        history: &impl TransactionHistoryStorage,
+    ) -> Result<(), Box<dyn Error>> {
+        match self {
+            Transaction::Deposit(transaction) => transaction.execute(account_storage, history),
+            Transaction::Withdrawal(transaction) => transaction.execute(account_storage, history),
+            Transaction::Dispute(transaction) => transaction.execute(account_storage, history),
+            Transaction::Resolve(transaction) => transaction.execute(account_storage, history),
+            Transaction::Chargeback(transaction) => transaction.execute(account_storage, history),
         }
-        storage.add_money(self.client_id, self.amount)
     }
 }
 
-pub struct Withdrawal {
-    client_id: UserId,
+impl TryFrom<&TransactionLogEntry> for Transaction {
+    type Error = TransactionLogError;
+
+    fn try_from(value: &TransactionLogEntry) -> Result<Self, Self::Error> {
+        let TransactionLogEntry {
+            transaction_type,
+            transaction_id,
+            client_id,
+            amount,
+        } = value;
+        match transaction_type.as_str() {
+            DEPOSIT_VALUE => {
+                let amount = amount.ok_or(TransactionLogError::MissingAmount)?;
+                Ok(Transaction::Deposit(Deposit {
+                    client_id: *client_id,
+                    transaction_id: *transaction_id,
+                    amount,
+                }))
+            }
+            WITHDRAWAL_VALUE => {
+                let amount = amount.ok_or(TransactionLogError::MissingAmount)?;
+                Ok(Transaction::Withdrawal(Withdrawal {
+                    client_id: *client_id,
+                    transaction_id: *transaction_id,
+                    amount,
+                }))
+            }
+            _ => panic!("not yet"),
+        }
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Deposit {
+    client_id: ClientId,
     transaction_id: TransactionId,
     amount: Decimal,
 }
 
-impl TransactionExecutor for Withdrawal {
-    fn execute(self, storage: &mut impl Storage) -> Result<(), Box<dyn Error>> {
+impl ExecTransaction for Deposit {
+    fn execute(
+        &self,
+        account_storage: &impl AccountStorage,
+        history: &impl TransactionHistoryStorage,
+    ) -> Result<(), Box<dyn Error>> {
         if self.amount.is_sign_negative() {
             return Err(Box::new(TransactionError::NegativeAmount));
         }
-        storage.withdraw_money(self.client_id, self.amount)
+        account_storage.add_money(self.client_id, self.amount)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Withdrawal {
+    client_id: ClientId,
+    transaction_id: TransactionId,
+    amount: Decimal,
+}
+
+impl ExecTransaction for Withdrawal {
+    fn execute(
+        &self,
+        account_storage: &impl AccountStorage,
+        history: &impl TransactionHistoryStorage,
+    ) -> Result<(), Box<dyn Error>> {
+        if self.amount.is_sign_negative() {
+            return Err(Box::new(TransactionError::NegativeAmount));
+        }
+        account_storage.withdraw_money(self.client_id, self.amount)
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Dispute {
+    client_id: ClientId,
+    transaction_id: TransactionId,
+}
+
+impl ExecTransaction for Dispute {
+    fn execute(
+        &self,
+        account_storage: &impl AccountStorage,
+        history: &impl TransactionHistoryStorage,
+    ) -> Result<(), Box<dyn Error>> {
+        Ok(())
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Resolve {
+    client_id: ClientId,
+    transaction_id: TransactionId,
+}
+
+impl ExecTransaction for Resolve {
+    fn execute(
+        &self,
+        account_storage: &impl AccountStorage,
+        history: &impl TransactionHistoryStorage,
+    ) -> Result<(), Box<dyn Error>> {
+        Ok(())
+    }
+}
+
+#[derive(Debug, PartialEq)]
+pub struct Chargeback {
+    client_id: ClientId,
+    transaction_id: TransactionId,
+}
+
+impl ExecTransaction for Chargeback {
+    fn execute(
+        &self,
+        account_storage: &impl AccountStorage,
+        history: &impl TransactionHistoryStorage,
+    ) -> Result<(), Box<dyn Error>> {
+        Ok(())
     }
 }
